630048df5a7d363b4c124ae8f007d414dfb10c35
EA <icyrealm@gmail.com>
2023-02-04 12:21:31 -0500

    Adjusted to enable npm run dev

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,15 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+    server: {
+        host: 'localhost',  // Add this to force IPv4 only
+        hmr: {
+            host: 'localhost'
+        },
+        // watch: {
+        //     usePolling: true,
+        // }
+    },
     plugins: [
         laravel({
             input: 'resources/js/app.jsx',
